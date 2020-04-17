5bad8182cfb3e590767b2475b02b154ec961b4d4
nicolasiragne <nicoragne@hotmail.fr>
2020-04-17 09:04:19 +0200

    yay

---
+++ string4.c
@@ -0,0 +1,7 @@
+//Compile with gcc
+//No null bytes in payload
+
+const char str[] = "salut les nazes\n";
+__attribute__((section(".text#")))
+char main[] = "л&A^IѓоUH‰е1яfїАIЌ4>1яяЗ1АяА1ТІ1А]ГиХяяя";
+
